e177228d5c7ce2298c9db3edcdc0c33b43fdc6aa
Philip <hi5pchiu@gmail.com>
2022-04-12 20:36:21 -0700

    Initial Commit

---
+++ Poli-Me1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Poli-Me1
+//
+//  Created by Philip Chiu on 4/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
